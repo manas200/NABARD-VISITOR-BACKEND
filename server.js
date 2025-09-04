e97fbb344ccee8bfb9f1547c77274fbf4398461c
manas200 <manasc2009@gmail.com>
2025-09-04 20:09:05 +0530

    cors issue fixed

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const cors = require("cors");
 const corsOptions = {
   origin: [
     "http://localhost:5173",
+    "http://localhost:5175",
     "https://nabard-visitor-frontend.vercel.app",
     "https://nabard-visitor-frontend-git-main-manas-chaturvedis-projects.vercel.app",
   ],
